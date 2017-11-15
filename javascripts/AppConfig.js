c059c27f855435c9ba390bf09513c95d78b6dfbf
Jess Brawner <jessnbillbrawner@gmail.com>
2017-11-14 18:06:51 -0600

    setup files and deployed through FB

---
+++ javascripts/AppConfig.js
@@ -0,0 +1,26 @@
+"use strict";
+
+app.run(function(FIREBASE_CONFIG){
+    firebase.initializeApp(FIREBASE_CONFIG);
+});
+
+app.config(function($routeProvider){
+    $routeProvider
+    .when("/auth", {
+        templateUrl: "partials/auth.html",
+        controller: "AuthCtrl"
+    })
+    .when("/contacts-favorites", {
+        templateUrl: "partials/contacts-favorites.html",
+        controller: "FavCtrl"
+    })
+    .when("/contacts-new", {
+        templateUrl: "partials/contacts-new.html",
+        controller: "NewCtrl"
+    })
+    .when("/contacts-view", {
+        templateUrl: "partials/contacts-view.html",
+        controller: "ViewCtrl"
+    })
+    .otherwise("/auth");
+});
---
+++ javascripts/app.js
@@ -0,0 +1,7 @@
+/* globals app: true */
+
+"use strict";
+
+const app = angular.module("ContactsApp", ["ngAnimate", "ngRoute", "ngSanitize"]);
+
+console.log("test", app);
